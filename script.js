b6d261c0de58bfa2dba7e39cd6c2c849f76e57e8
TanyaS_Parihar <tanyasp666@gmail.com>
2024-08-04 13:30:28 +0530

    Submission  commit

---
+++ script.js
@@ -0,0 +1,68 @@
+const button = document.querySelector(".button");
+const nextquote = document.querySelector(".nextquote");
+
+button.addEventListener("click", () => {
+  const body = document.body;
+  const currentColor = body.style.backgroundColor;
+  const title = document.querySelector(".title");
+  if (body.style.backgroundColor == "rgb(24, 9, 39)") {
+    body.style.backgroundColor = "lightblue";
+    body.style.color = "black";
+    title.style.color = "olivedrab";
+  } else {
+    body.style.backgroundColor = "rgb(24, 9, 39)";
+    body.style.color = "rgba(245, 245, 245, 0.876)";
+    title.style.color = "rgb(196, 16, 16)";
+  }
+});
+const quotes = () => {
+  const fetchedQuotes = fetch("https://zenquotes.io/api/random/")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+
+      document.querySelector(".quote").innerHTML = `"${data.map(
+        (data) => data.q
+      )}"`;
+      document.querySelector(".author").innerHTML = `-${data.map(
+        (data) => data.a
+      )}`;
+    });
+};
+
+quotes();
+nextquote.addEventListener("click", () => {
+  quotes();
+});
+
+const quotes2 = () => {
+  const fetchedQuotes = fetch("https://zenquotes.io/api/quotes")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+
+      document.querySelector(".q").innerHTML = `"${data.map(
+        (data) => data.h + "\n"
+      )}"`;
+    });
+};
+quotes2();
+
+// const fetchedQuotes2 = fetch("https://zenquotes.io/api/quotes/")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log(data);
+
+//     document.querySelector(".quote-list").innerHTML = `-${data.map(
+//       (data) => data.h
+//     )}`;
+//   });
+
+const names = document.getElementById("names");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const country = document.getElementById("country");
+
+if (names.value.trim() == " ") {
+  document.querySelector(" ");
+}
